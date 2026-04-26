8eac7693d9382804f85a982998c3c66c746d68ec
Andras Baki <andrasbaki6@gmail.com>
2026-04-26 22:30:18 +0200

    Add pyinstaller setup to server-build/setup

--- server-build/setup.sh
+++ server-build/setup.sh
@@ -1,4 +1,4 @@
-pip3 install fastapi uvicorn
+pip3 install fastapi uvicorn pyinstaller
 
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
