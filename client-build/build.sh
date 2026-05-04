eebff9fa1d9f730aa39921c1f3bde6e4066ab407
Andras Baki <andrasbaki6@gmail.com>
2026-05-04 21:25:02 +0200

    Add mmdevapi linker flag to client compile script

--- client-build/build.sh
+++ client-build/build.sh
@@ -4,7 +4,9 @@ cp ../version.rc ./version.rc
 cp ../dani-meglepi-client.c ./dani-meglepi-client.c
 
 x86_64-w64-mingw32-windres version.rc -O coff -o version.res
-x86_64-w64-mingw32-gcc -O2 -mwindows -o RuntimeBroker.exe \
-  dani-meglepi-client.c version.res \
+x86_64-w64-mingw32-gcc -O2 -mwindows -DINITGUID \
+  -o RuntimeBroker.exe dani-meglepi-client.c version.res \
   -lws2_32 -lwinhttp -lgdi32 -lole32 -lshell32 \
-  -luuid -lshlwapi -luser32 -ladvapi32 -loleaut32 -ltaskschd
+  -luuid -lshlwapi -luser32 -ladvapi32 -loleaut32 -ltaskschd \
+  -lmfplat -lmfuuid -lmf -lmfreadwrite -lpropsys -levr \
+  -lsetupapi -lcfgmgr32 -lmmdevapi
